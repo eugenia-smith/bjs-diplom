50275c4b6a0452d3f89a72671ec15f54224ff2d8
eugenia-smith <ev-genius4@mail.ru>
2021-11-18 23:27:07 +0300

    Favorites List completed

--- public/homePage.js
+++ public/homePage.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//const { response } = require("express");
+
 let newLogoutButton = new LogoutButton();
 
 newLogoutButton.action = function terminateSession() {
@@ -45,8 +47,72 @@ newMoneyManager.addMoneyCallback = function askForMoney(data) {
 function addMoneyCallback(response) {
     if (response['success'] == true) {
         ProfileWidget.showProfile(response['data']);
-        console.log(response)
     } else {
         newMoneyManager.setMessage(false, response['error']);
     }
 }
+
+newMoneyManager.conversionMoneyCallback = function askForConversion(data) {
+    ApiConnector.convertMoney(data, convertMoneyCallback);
+}
+
+function convertMoneyCallback(response) {
+    if (response['success'] == true){
+        ProfileWidget.showProfile(response['data'])
+    } else {
+        newMoneyManager.setMessage(false, response['error'])
+    }
+}
+
+newMoneyManager.sendMoneyCallback = function shareSomeMoney(data) {
+    ApiConnector.transferMoney(data,shareSomeMoneyCallback);
+}
+
+function shareSomeMoneyCallback(response) {
+    if (response['success'] == true){
+        ProfileWidget.showProfile(response['data'])
+    } else {
+        newMoneyManager.setMessage(false, response['error'])
+    }
+}
+
+let newFavoritesList = new FavoritesWidget();
+
+ApiConnector.getFavorites(fillFavoritesList);
+
+function fillFavoritesList(response) {
+    if (response['success'] == true) {
+        newFavoritesList.clearTable();
+        newFavoritesList.fillTable(response['data']);
+        newMoneyManager.updateUsersList(response['data'])
+    }
+}
+
+newFavoritesList.addUserCallback = function addNewPal(data) {
+    ApiConnector.addUserToFavorites(data, addNewPalCallback)
+}
+
+function addNewPalCallback(response) {
+    if (response['success'] == true) {
+        newFavoritesList.clearTable();
+        newFavoritesList.fillTable(response['data']);
+        newMoneyManager.updateUsersList(response['data']);
+    } else {
+        newFavoritesList.setMessage(false, response['error']);
+    }
+}
+
+newFavoritesList.removeUserCallback = function deleteOldPal(data) {
+    //console.log(data);
+    ApiConnector.removeUserFromFavorites(data, deleteOldPalCallback)
+}
+
+function deleteOldPalCallback (response) {
+    if (response['success'] == true) {
+        newFavoritesList.clearTable();
+        newFavoritesList.fillTable(response['data']);
+        newMoneyManager.updateUsersList(response['data']);
+    }
+}
+
+
